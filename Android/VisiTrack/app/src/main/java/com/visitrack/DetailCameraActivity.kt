2c2be065c37e96ea3d6b62c8dc56becf16fbe303
Aldy Hermawan <41948718+aldyhermawan1@users.noreply.github.com>
2021-05-26 23:29:11 +0800

    Finished Designing DetailNotification

---
+++ Android/VisiTrack/app/src/main/java/com/visitrack/DetailCameraActivity.kt
@@ -0,0 +1,11 @@
+package com.visitrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailCameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_camera)
+    }
+}
