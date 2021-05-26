ac6668a62a8dc48e1913d1d8f24967ed731c7221
Aldy Hermawan <41948718+aldyhermawan1@users.noreply.github.com>
2021-05-27 00:16:45 +0800

    Finished Designing DetailCamera

--- Android/VisiTrack/app/src/main/java/com/visitrack/DetailCameraActivity.kt
+++ Android/VisiTrack/app/src/main/java/com/visitrack/DetailCameraActivity.kt
@@ -2,10 +2,13 @@ package com.visitrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.visitrack.databinding.ActivityDetailCameraBinding
 
 class DetailCameraActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailCameraBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_camera)
+        binding = ActivityDetailCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
