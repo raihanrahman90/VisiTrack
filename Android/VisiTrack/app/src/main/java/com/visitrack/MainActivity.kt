d5308bab4e61111b77ed99dbc8abddea1f825f86
yulistiana1107 <yulistianaprasetya@gmail.com>
2021-05-29 18:31:31 +0700

    toolbar

--- Android/VisiTrack/app/src/main/java/com/visitrack/MainActivity.kt
+++ Android/VisiTrack/app/src/main/java/com/visitrack/MainActivity.kt
@@ -10,5 +10,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setUpTollbarVisitrack()
+        setUpTollbarTitleVisitrack(resources.getString((R.string.toolbar_visitrack)))
+    }
+
+    private fun setUpTollbarVisitrack(){
+        setSupportActionBar(binding.visitrackToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun setUpTollbarTitleVisitrack(title: String){
+        supportActionBar?.title = title
     }
 }
