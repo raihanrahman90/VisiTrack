d93d98a49c27b1892fc42cffcabcda03a03697a7
yulistiana1107 <yulistianaprasetya@gmail.com>
2021-05-29 18:07:00 +0700

    Login to Register

--- Android/VisiTrack/app/src/main/java/com/visitrack/LoginActivity.kt
+++ Android/VisiTrack/app/src/main/java/com/visitrack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.visitrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
@@ -7,11 +8,15 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.visitrack.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.contentLogin.registerHere.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
