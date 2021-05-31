ab722465395609c2403e9ee6886f6cf418b9d424
yulistiana1107 <yulistianaprasetya@gmail.com>
2021-05-31 15:06:39 +0700

    Edt Login and Register

--- Android/VisiTrack/app/src/main/java/com/visitrack/start/ui/LoginActivity.kt
+++ Android/VisiTrack/app/src/main/java/com/visitrack/start/ui/LoginActivity.kt
@@ -3,18 +3,37 @@ package com.visitrack.start.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.visitrack.R
 import com.visitrack.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.contentLogin.btnLogin.setOnClickListener(this)
+
         binding.contentLogin.registerHere.setOnClickListener{
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+
+            R.id.btn_login -> {
+                val passwords = binding.contentLogin.edtPassword.text.toString().trim()
+                val username = binding.contentLogin.edtUsername.text.toString().trim()
+                if (username.equals("")) {
+                    binding.contentLogin.edtUsername.error = "Please Enter Your Username"
+                } else if (passwords.equals("")) {
+                    binding.contentLogin.edtPassword.error = "Please Enter Your Password"
+                }
+            }
         }
     }
 }
--- Android/VisiTrack/app/src/main/java/com/visitrack/start/ui/RegisterActivity.kt
+++ Android/VisiTrack/app/src/main/java/com/visitrack/start/ui/RegisterActivity.kt
@@ -2,13 +2,41 @@ package com.visitrack.start.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.visitrack.R
 import com.visitrack.databinding.ActivityRegisterBinding
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.contentRegister.btnRegister.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+
+            R.id.btn_login -> {
+                val passwords = binding.contentRegister.edtPassword.text.toString().trim()
+                val username = binding.contentRegister.edtUsername.text.toString().trim()
+                val email = binding.contentRegister.edtEmail.text.toString().trim()
+                val fullName = binding.contentRegister.edtFullName.text.toString().trim()
+                val company = binding.contentRegister.edtCompany.text.toString().trim()
+                if (username.equals("")) {
+                    binding.contentRegister.edtUsername.error = "Please Enter Your Username"
+                } else if (passwords.equals("")) {
+                    binding.contentRegister.edtPassword.error = "Please Enter Your Password"
+                } else if (email.equals("")) {
+                    binding.contentRegister.edtEmail.error = "Please Enter Your Email"
+                } else if (fullName.equals("")) {
+                    binding.contentRegister.edtFullName.error = "Please Enter Your Full Name"
+                } else if (company.equals("")) {
+                    binding.contentRegister.edtCompany.error = "Please Enter Your Company"
+                }
+            }
+        }
     }
 }
