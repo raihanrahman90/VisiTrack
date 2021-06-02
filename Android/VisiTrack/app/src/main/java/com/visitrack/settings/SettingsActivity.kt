1c220d0eee99c2e06650d803fbbf18ccdbbd1f91
yulistiana1107 <yulistianaprasetya@gmail.com>
2021-06-02 22:43:55 +0700

    Setting doneeeee

--- Android/VisiTrack/app/src/main/java/com/visitrack/settings/SettingsActivity.kt
+++ Android/VisiTrack/app/src/main/java/com/visitrack/settings/SettingsActivity.kt
@@ -1,6 +1,5 @@
 package com.visitrack.settings
 
-import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
@@ -8,22 +7,20 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.View
 import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.edit
 import com.visitrack.R
 import com.visitrack.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivitySettingsBinding
     companion object {
-        private const val Visitrack_REMINDER = "Visitrack_pref"
+//        private const val Visitrack_REMINDER = "Visitrack_pref"
     }
     private lateinit var Visitrackpreference: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setNotification(this)
+//        setNotification(this)
 
         setUpToolbarVisitrack()
         setUpToolbarTitleVisitrack(resources.getString((R.string.setting)))
@@ -35,26 +32,25 @@ class SettingsActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun setUpToolbarVisitrack(){
         setSupportActionBar(binding.settingToolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     private fun setUpToolbarTitleVisitrack(title: String){
         supportActionBar?.title = title
     }
 
-    private fun setNotification(context: Context) {
-        Visitrackpreference = getSharedPreferences(Visitrack_REMINDER, Context.MODE_PRIVATE)
-        binding.swDaily.apply {
-            isChecked = Visitrackpreference.getBoolean(Visitrack_REMINDER, false)
-            setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked) {
-                    Visitrackpreference.edit { putBoolean(Visitrack_REMINDER, true) }
-                } else {
-                    Visitrackpreference.edit { putBoolean(Visitrack_REMINDER, false) }
-                }
-            }
-        }
-    }
+//    private fun setNotification(context: Context) {
+//        Visitrackpreference = getSharedPreferences(Visitrack_REMINDER, Context.MODE_PRIVATE)
+//        binding.swDaily.apply {
+//            isChecked = Visitrackpreference.getBoolean(Visitrack_REMINDER, false)
+//            setOnCheckedChangeListener { _, isChecked ->
+//                if (isChecked) {
+//                    Visitrackpreference.edit { putBoolean(Visitrack_REMINDER, true) }
+//                } else {
+//                    Visitrackpreference.edit { putBoolean(Visitrack_REMINDER, false) }
+//                }
+//            }
+//        }
+//    }
 
     override fun onClick (v: View?) {
         when (v?.id) {
